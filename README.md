4ab7877711df365133331395bba48a7471f49cc7
Matthew Upp <1350411+MatthewUpp@users.noreply.github.com>
2020-05-31 07:04:02 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
-# OctoPrint-EEPROM-MPSelectMini
+# OctoPrint-EEPROM-Malyan-STM32F401
 
-This plugin is designed to get, change and save the values in the EEPROM of your Monoprice Select Mini or Malyan M200 3D Printer.
+This plugin is designed to get, change and save the values in the EEPROM of your Malyan or Monoprice STM32F401 Based 3D Printer.
 
 ## Setup
 
 Install via the bundled [Plugin Manager](https://github.com/foosel/OctoPrint/wiki/Plugin:-Plugin-Manager)
-manually using this URL:
-
-    https://github.com/bruhmann/OctoPrint-EEPROM-MPSelectMini/archive/master.zip
+or
+Manually using this URL: https://github.com/MatthewUpp/OctoPrint-EEPROM-Malyan-STM32F401/archive/master.zip
--- setup.py
+++ setup.py
@@ -4,30 +4,30 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "eeprom_MPSelectMini"
+plugin_identifier = "eeprom_malyan_stm32f401"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_eeprom_MPSelectMini"
+plugin_package = "octoprint_eeprom_malyan_stm32f401"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-EEprom-MonopriceSelectMini"
+plugin_name = "OctoPrint-EEPROM-Malyan-STM32F401"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.1"
+plugin_version = "0.1.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """Makes it possible to change the EEPROM values of Monoprice Select Mini 3D and Malyan M200 Firmware through OctoPrint based on OctoPrint-EEprom-Marlin by amsbr"""
+plugin_description = """Makes it possible to change the EEPROM values of Malyan & Monoprice STM32F401 Based 3D Printers through OctoPrint based on OctoPrint-EEPROM-MPSelectMini by bruhmann"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Brian Ruhmann"
+plugin_author = "Matthew Upp"
 
 # The plugin's author's mail address.
-plugin_author_email = "ruhmann@gmail.com"
+plugin_author_email = "matthew@gigdigit.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/bruhmann/OctoPrint-EEPROM-MPSelectMini"
+plugin_url = "https://github.com/MatthewUpp/OctoPrint-EEPROM-Malyan-STM32F401"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
